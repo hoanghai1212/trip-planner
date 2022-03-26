20c3608574acfe6507da4a6213253769cf3efb7d
Hoang Hai <hoanghai129612@gmail.com>
2022-03-26 11:42:04 +0700

    refactor: setup project base structure

---
+++ src/shared/interfaces/global/environment.d.ts
@@ -0,0 +1,11 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      PORT?: string;
+      PUBLIC_URL: string;
+    }
+  }
+}
+
+export {};
