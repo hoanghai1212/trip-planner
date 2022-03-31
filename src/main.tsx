4fe93502054b4078a1286d4b2c424ffe50bdad8c
Hoang Hai <hoanghai129612@gmail.com>
2022-03-31 13:05:33 +0700

    chore: bump react, react-dom version to '18.0.0'

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import '@/shared/scss/main.scss';
 
 import App from './app';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+
+const root = container && createRoot(container);
+
+root?.render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
